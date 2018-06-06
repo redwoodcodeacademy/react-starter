661538c7b571366e732b43351d4583a9a31de204
Harrison Spain <harrison.spain@redwoodcodeacademy.com>
2018-06-06 13:35:40 -0700

    initial

---
+++ src/components/container.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+         )
+    }
+}
+ 
+export default Container;
